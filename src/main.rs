b15f50b70bfd4fa8c35adfa9b479c8fa42fc8924
satyamacn <satyam.arya@accenture.com>
2023-02-09 15:38:15 +0000

    1.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn add(a:i32,b:i32) -> i32 {
+    a+b
+}
+
+#[test]
+fn test_addition(){
+    assert_eq!(add(2,3),5);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = add(3,5);
+    println!("the result is: {}",result);
 }
